72cddbfb4ec5d9c0caa14578c4a87d57aa729056
TepidmishA <perep.main@mail.ru>
2025-11-13 09:35:52 +0300

    remove MPI_Scatterv

--- tasks/perepelkin_i_string_diff_char_count/mpi/src/ops_mpi.cpp
+++ tasks/perepelkin_i_string_diff_char_count/mpi/src/ops_mpi.cpp
@@ -6,7 +6,6 @@
 #include <cstddef>
 #include <functional>
 #include <numeric>
-#include <vector>
 
 #include "perepelkin_i_string_diff_char_count/common/include/common.hpp"
 
@@ -38,31 +37,27 @@ bool PerepelkinIStringDiffCharCountMPI::RunImpl() {
   const size_t min_len = std::min(len1, len2);
   const size_t max_len = std::max(len1, len2);
 
-  // Prepare distribution of indices across processes
   const size_t base_size = min_len / proc_num;
   const int remainder = static_cast<int>(min_len % proc_num);
-  std::vector<int> counts(proc_num);
-  std::vector<int> displacements(proc_num);
-  for (int i = 0, offset = 0; i < proc_num; i++) {
-    counts[i] = static_cast<int>(base_size + (i < remainder ? 1 : 0));
-    displacements[i] = offset;
-    offset += counts[i];
+
+  // Calculate local range for this process
+  size_t local_start = 0;
+  size_t local_end = 0;
+  for (int i = 0; i < proc_num; i++) {
+    size_t current_size = base_size + (i < remainder ? 1 : 0);
+    if (i == proc_rank) {
+      local_start = local_end;
+      local_end = local_start + current_size;
+      break;
+    }
+    local_end += current_size;
   }
 
-  // Allocate local buffers
-  const int local_size = counts[proc_rank];
-  std::vector<char> local_s1(local_size);
-  std::vector<char> local_s2(local_size);
-
-  // Scatter parts of the strings to processes
-  MPI_Scatterv(s1.data(), counts.data(), displacements.data(), MPI_CHAR, local_s1.data(), local_size, MPI_CHAR, 0,
-               MPI_COMM_WORLD);
-  MPI_Scatterv(s2.data(), counts.data(), displacements.data(), MPI_CHAR, local_s2.data(), local_size, MPI_CHAR, 0,
-               MPI_COMM_WORLD);
-
   // Compute local number of differing characters
-  int local_diff = std::transform_reduce(local_s1.begin(), local_s1.end(), local_s2.begin(), 0, std::plus<>(),
-                                         std::not_equal_to<>());
+  auto s1_start = s1.begin() + static_cast<std::string::difference_type>(local_start);
+  auto s1_end = s1.begin() + static_cast<std::string::difference_type>(local_end);
+  auto s2_start = s2.begin() + static_cast<std::string::difference_type>(local_start);
+  int local_diff = std::transform_reduce(s1_start, s1_end, s2_start, 0, std::plus<>(), std::not_equal_to<>());
 
   // Reduce (sum) differences for the common parts
   int global_diff = 0;
