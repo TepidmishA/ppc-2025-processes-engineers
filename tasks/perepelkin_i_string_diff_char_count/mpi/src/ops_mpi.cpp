814685ec6e846c005963a81373695b651b3ff13c
TepidmishA <perep.main@mail.ru>
2025-11-23 18:33:47 +0300

    implement mpi input data distributions

--- tasks/perepelkin_i_string_diff_char_count/mpi/src/ops_mpi.cpp
+++ tasks/perepelkin_i_string_diff_char_count/mpi/src/ops_mpi.cpp
@@ -13,8 +13,13 @@
 namespace perepelkin_i_string_diff_char_count {
 
 PerepelkinIStringDiffCharCountMPI::PerepelkinIStringDiffCharCountMPI(const InType &in) {
+  MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank_);
+  MPI_Comm_size(MPI_COMM_WORLD, &proc_num_);
+
   SetTypeOfTask(GetStaticTypeOfTask());
-  GetInput() = in;
+  if (proc_rank_ == 0) {
+    GetInput() = in;
+  }
   GetOutput() = 0;
 }
 
@@ -27,37 +32,64 @@ bool PerepelkinIStringDiffCharCountMPI::PreProcessingImpl() {
 }
 
 bool PerepelkinIStringDiffCharCountMPI::RunImpl() {
-  int proc_rank = 0;
-  int proc_num = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+  int min_len = 0;
+  int max_len = 0;
 
-  const auto &[s1, s2] = GetInput();
-  const size_t len1 = s1.size();
-  const size_t len2 = s2.size();
-  const size_t min_len = std::min(len1, len2);
-  const size_t max_len = std::max(len1, len2);
+  if (proc_rank_ == 0) {
+    const auto &[s1, s2] = GetInput();
+    min_len = static_cast<int>(std::min(s1.size(), s2.size()));
+    max_len = static_cast<int>(std::max(s1.size(), s2.size()));
+  }
 
-  const size_t base_size = min_len / proc_num;
-  const int remainder = static_cast<int>(min_len % proc_num);
+  MPI_Bcast(&min_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Bcast(&max_len, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-  // Calculate local range for this process
-  const size_t local_size = base_size + (proc_rank < remainder ? 1 : 0);
-  const size_t local_start = (proc_rank * base_size) + std::min(proc_rank, remainder);
-  const size_t local_end = local_start + local_size;
+  std::vector<char> local_s1;
+  std::vector<char> local_s2;
+  DistributeData(min_len, local_s1, local_s2);
 
-  // Compute local number of differing characters
-  auto s1_start = s1.begin() + static_cast<std::string::difference_type>(local_start);
-  auto s1_end = s1.begin() + static_cast<std::string::difference_type>(local_end);
-  auto s2_start = s2.begin() + static_cast<std::string::difference_type>(local_start);
-  int local_diff = std::transform_reduce(s1_start, s1_end, s2_start, 0, std::plus<>(), std::not_equal_to<>());
+  int local_diff = std::transform_reduce(local_s1.begin(), local_s1.end(), local_s2.begin(), 0, std::plus<>(),
+                                         std::not_equal_to<>());
 
-  // Reduce (sum) differences for the common parts
   int global_diff = 0;
   MPI_Allreduce(&local_diff, &global_diff, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
   GetOutput() = global_diff + static_cast<int>(max_len - min_len);
   return true;
+}
+
+void PerepelkinIStringDiffCharCountMPI::DistributeData(int min_len, std::vector<char> &local_s1,
+                                                       std::vector<char> &local_s2) {
+  const int base_size = min_len / proc_num_;
+  const int remainder = min_len % proc_num_;
+
+  std::vector<int> counts(proc_num_);
+  std::vector<int> displacements(proc_num_);
+
+  if (proc_rank_ == 0) {
+    for (int i = 0, offset = 0; i < proc_num_; i++) {
+      counts[i] = base_size + (i < remainder ? 1 : 0);
+      displacements[i] = offset;
+      offset += counts[i];
+    }
+  }
+
+  const int local_size = base_size + (proc_rank_ < remainder ? 1 : 0);
+  local_s1.resize(local_size);
+  local_s2.resize(local_size);
+
+  const char *s1_data = nullptr;
+  const char *s2_data = nullptr;
+  if (proc_rank_ == 0) {
+    const auto &[s1, s2] = GetInput();
+    s1_data = s1.data();
+    s2_data = s2.data();
+  }
+
+  MPI_Scatterv(s1_data, counts.data(), displacements.data(), MPI_CHAR, local_s1.data(), local_size, MPI_CHAR, 0,
+               MPI_COMM_WORLD);
+  MPI_Scatterv(s2_data, counts.data(), displacements.data(), MPI_CHAR, local_s2.data(), local_size, MPI_CHAR, 0,
+               MPI_COMM_WORLD);
 }
 
 bool PerepelkinIStringDiffCharCountMPI::PostProcessingImpl() {
